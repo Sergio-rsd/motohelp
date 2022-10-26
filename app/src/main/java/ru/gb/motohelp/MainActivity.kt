493348c0aa45eb0b48347bcbf14c9ebd22d9d22f
Victor <tashev.victor@gmail.com>
2022-10-26 14:16:52 +0300

    Добавил заготовку для аутентификации

--- app/src/main/java/ru/gb/motohelp/MainActivity.kt
+++ app/src/main/java/ru/gb/motohelp/MainActivity.kt
@@ -4,14 +4,38 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import ru.gb.motohelp.ui.fragments.MapScreenFragment
 
 lateinit var sharedPreferences: SharedPreferences
 lateinit var sharedPreferencesEditor: SharedPreferences.Editor
 
+//private lateinit var oneTapClient: SignInClient
+//private lateinit var signInRequest: BeginSignInRequest
+
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.your_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()*/
+
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
